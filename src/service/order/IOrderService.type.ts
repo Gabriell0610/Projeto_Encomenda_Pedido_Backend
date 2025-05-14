13696253217e70e18206d76373a609331f076887
Gabriell0610 <gabrielbarbosaa432@gmail.com>
2025-05-14 18:03:41 -0300

    fix: fix prisma

--- src/service/order/IOrderService.type.ts
+++ src/service/order/IOrderService.type.ts
@@ -3,7 +3,7 @@ import { OrderDto, UpdateOrderDto } from "@/domain/dto/order/OrderDto";
 
 interface IOrderService {
   createOrder: (order: OrderDto) => Promise<{ id: string }>;
-  updateOrder: (id: string, order: UpdateOrderDto) => Promise<OrderEntity>;
+  updateOrder: (id: string, order: UpdateOrderDto) => Promise<Partial<OrderEntity>>;
   cancelOrder: (id: string) => Promise<{ id: string }>;
   listOrdersByClientId: (idClient: string) => Promise<Partial<OrderEntity>[]>;
   listAllOrders: () => Promise<Partial<OrderEntity>[]>;
